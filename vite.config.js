9a02bbacecec1b6ceecd0640d93fd7ef35c1f59c
PyRo1121 <olen@latham.cloud>
2023-07-01 23:51:22 -0400

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,27 @@ export default defineConfig({
     manifest: true,
     sourcemap: false,
     minify: 'terser', // Use Terser for minification
+    terserOptions: {
+      compress: {
+        passes: 2, // Run the compressor multiple times
+        module: true, // Optimize for ES6 modules
+        unused: true, // Drop unreferenced functions/variables
+        dead_code: true, // Remove unreachable code
+        hoist_funs: true, // Hoist function declarations
+        join_vars: true, // Join consecutive var statements
+      },
+      output: {
+        comments: false, // Remove comments from output
+      },
+      mangle: {
+        // Optionally mangle properties.
+        properties: {
+          regex: /^_/, // Mangle property names with _ prefix
+        },
+      },
+    },
     rollupOptions: {
+      treeshake: true, // Enable tree-shaking
       input: {
         main: './index.html',
       },
@@ -24,6 +44,7 @@ export default defineConfig({
       },
     },
     chunkSizeWarningLimit: 500,
+    cacheDir: '.vite_cache', // Specify a cache directory
   },
   optimizeDeps: {
     include: ['solid-js', 'solid-js/web'],
