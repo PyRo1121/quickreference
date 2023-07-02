06c5a2905539512a3b699378678ae433bb6d55dc
PyRo1121 <olen@latham.cloud>
2023-07-01 22:22:03 -0400

    updated vite config for cloudflare

--- vite.config.js
+++ vite.config.js
@@ -11,19 +11,28 @@ export default defineConfig({
     assetsDir: 'assets',
     emptyOutDir: true,
     manifest: true,
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-    },
-    minify: 'terser',
-    sourcemap: true,
-    brotliSize: true,
-    chunkSizeWarningLimit: 2000,
+    sourcemap: false, // Disabling sourcemaps for production
     terserOptions: {
       compress: {
         drop_console: true,
       },
     },
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+      output: {
+        entryFileNames: '[name].[hash].js', // Adding hash to the output file names for cache busting
+        chunkFileNames: '[name].[hash].js',
+        assetFileNames: '[name].[hash].[ext]',
+      },
+    },
+    chunkSizeWarningLimit: 500, // Adjusting the chunk size warning limit based on your application's needs
+  },
+  optimizeDeps: {
+    include: ['solid-js', 'solid-js/web'], // Explicitly include SolidJS and its web runtime to optimize dependencies
+  },
+  esbuild: {
+    jsxInject: `import { createSignal, Show, lazy } from 'solid-js';`, // Injecting SolidJS imports for ESBuild optimization
   },
 });
